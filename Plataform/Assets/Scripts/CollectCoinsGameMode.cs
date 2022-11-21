f6a01e94c6ce8eb5b081f2628c03e766059e9b92
Lipe <b.luiz@escolar.ifrn.edu.br>
2022-11-21 12:23:30 -0300

    Atualizando o diretório
    ...

---
+++ Plataform/Assets/Scripts/CollectCoinsGameMode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    [CreateAssetMenu(fileName = "CollectCoinsGameMode", menuName = "GameModes/Collect Coins", order = 0)]
+    public class CollectCoinsGameMode : GameMode
+    {
+        public int CoinsToCollect;
+
+        public override void UpdateGameMode(int coins = 0, float time = 0, bool arrivedAtLocation = false)
+        {
+            if (coins >= CoinsToCollect)
+            {
+                gameState = GameState.Victory;
+                OnGameModeStateChanged?.Invoke(gameState);
+            }
+        }
+    }
+}
--- Plataform/Assets/Scripts/GameManager.cs
+++ Plataform/Assets/Scripts/GameManager.cs
@@ -1,27 +1,67 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Management.Instrumentation;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityTemplateProjects;
 
-/// <sumarry>
+/// <summary>
+/// Estados do Game Manager
+/// </summary>
+public enum GameState
+{
+    Initialization,
+    Running,
+    Victory,
+    GameOver
+}
+
+/// <summary>
 /// Classe usada para gerenciar o jogo
-/// </sumarry>
-
+/// </summary>
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance; // instância do singleton
+    public static GameManager Instance; // instancia do singleton
 
-    [SerializeField]
-    private string guiName; // Name da fase da interface
+    public GameMode gameMode;
+    public GameState GameState
+    {
+        get => _gameState;
+        set
+        {
+            if (value == _gameState) return;
+            _gameState = value;
+            OnGameStateChanged();
+        }
+    }
+    
+    public int coinsToWin;
+    public float timeToLose;
+    
+    [SerializeField] 
+    private string guiName; // nome da fase de interface
 
-    [SerializeField]
-    private string levelName; // Nome da fase do jogo
+    [SerializeField] 
+    private string levelName; // nome da fase de jogo
 
-    [SerializeField]
-    private GameObject playerAndCameraPrefab; // Referência para o Prefab do jogador + câmera
+    [SerializeField] 
+    private GameObject playerAndCameraPrefab; // referencia pro prefab do jogador + camera
 
-    // Start is called before the first frame update
+    private GameState _gameState; // variavel que guarda o estado atual do game manager
+    private float _currentTime;
+    private void OnEnable()
+    {
+        PlayerObserverManager.OnCoinsChanged += PlayerCoinsUpdate;
+        gameMode.OnGameModeStateChanged += HandleGameMode;
+    }
+    private void OnDisable()
+    {
+        PlayerObserverManager.OnCoinsChanged -= PlayerCoinsUpdate;
+        gameMode.OnGameModeStateChanged -= HandleGameMode;
+    }
+
     private void Awake()
     {
         // Condição de criação do singleton
@@ -29,8 +69,8 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             
-            // Impede que o objeto indicado entre parênteses seja destruido
-            DontDestroyOnLoad(this.gameObject); //referência para o objeto que contém o GameManager
+            // Impede que o objeto indicado entre parenteses seja destruido
+            DontDestroyOnLoad(this.gameObject); // referencia pro objeto que contem o GameManager
         }
         else Destroy(this.gameObject);
     }
@@ -41,77 +81,165 @@ public class GameManager : MonoBehaviour
         // Se estiver iniciando a cena a partir de Initialization, carregue o jogo
         // do jeito de antes
         if (SceneManager.GetActiveScene().name == "Initialization")
-        {
             StartGameFromInitialization();
-        }
-        // Caso contrário, está iniciando a partir do level, carregue o jogo do modo apropriado
-        else
-        {
+        else // caso contrario, esta iniciando a partir do level, carregue o jogo
+             // do modo apropriado
             StartGameFromLevel();
-        }
     }
 
     private void StartGameFromLevel()
     {
-        // 1 - carrega a cena de interface de modo aditivo para não apagar a cena do level
-        // da memória RAM
+        // 1 - Carrega a cena de interface de modo aditivo para nao apagar a cena do level
+        // da memoria ram
         SceneManager.LoadScene(guiName, LoadSceneMode.Additive);
         
         // 2 - Precisa instanciar o jogador na cena
-        //Começa procurando o objeto PlayerStart na Cena do level
+        // começa procurando o objeto PlayerStart na cena do level
         Vector3 playerStartPosition = GameObject.Find("PlayerStart").transform.position;
-
-        // Instancia o Prefab do jogador na posição player start com rotação zerada
-        Instantiate(original: playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
+    
+        // instancia o prefab do jogador na posicao do player start com rotação zerada
+        Instantiate(playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
         
         // 3 - Inicia o jogo
+        // coloca o estado inicial como Running (Rodando)
+        GameState = GameState.Running;
     }
-
+    
     public void StartGame()
     {
         // 1 - Carregar a cena de interface e do jogo
         SceneManager.LoadScene(guiName);
-        // SceneManager.LoadScene(levelName, LoadSceneMode.Additive); // Additive carrega uma
+        //SceneManager.LoadScene(levelName, LoadSceneMode.Additive); // Additive carrega uma nova cena sem descarregar a anterior
 
         SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive).completed += operation =>
         {
-            // Inicializa a variável para guardar a cena do level com o valor padrão (default)
+            // Inicializa a variavel para guardar a cena do level com o valor padrao (default)
             Scene levelScene = default;
-
-            // Encontrar a cena de level que está carregada
-            // O "for" que intera no array de cenas abertas
+            
+            // encontrar a cena de level que está carregada
+            // for que itera no array de cenas abertas
             for (int i = 0; i < SceneManager.sceneCount; i++)
             {
-                // Se o nome da cena na posição i do array for igual ao nome do level
+                // se o nome da cena na posição i do array for igual ao nome do level
                 if (SceneManager.GetSceneAt(i).name == levelName)
                 {
-                    // Associe a cena na posição i do array à variável
+                    // associe a cena na posição i do array à variável 
                     levelScene = SceneManager.GetSceneAt(i);
                     break;
                 }
             }
-
-            // Se a variável estiver com um valor diferente do padrão, significa que ela foi alterada
-            // e a cena do level atual foi encontrada no array, então faça ela ser a nova cena ativa.
-            if (levelScene != default) SceneManager.SetActiveScene(levelScene);
-
+            // se a variável tiver um valor diferente do padrão, significa que ela foi alterada
+            // e a cena do level atual foi encontrada no array, entao faça ela ser a
+            // nova cena ativa
+            if(levelScene != default) SceneManager.SetActiveScene(levelScene);
+            
             // 2 - Precisa instanciar o jogador na cena
-            //Começa procurando o objeto PlayerStart na Cena do level
+            // começa procurando o objeto PlayerStart na cena do level
             Vector3 playerStartPosition = GameObject.Find("PlayerStart").transform.position;
-
-            // Instancia o Prefab do jogador na posição player start com rotação zerada
-            Instantiate(original: playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
+    
+            // instancia o prefab do jogador na posicao do player start com rotação zerada
+            Instantiate(playerAndCameraPrefab, playerStartPosition, Quaternion.identity);
+            
             // 3 - Começar a partida
+            GameState = GameState.Running;
         };
+
     }
 
     private void StartGameFromInitialization()
     {
-        SceneManager.LoadScene(("Splash"));
+        GameState = GameState.Initialization;
+        SceneManager.LoadScene("Splash");
     }
 
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+    
+    private void PlayerCoinsUpdate(int obj)
+    {
+        //if (obj >= coinsToWin) GameState = GameState.Victory;
+        gameMode.UpdateGameMode(obj);
+    }
+
+    private void OnGameStateChanged()
+    {
+        switch (_gameState)
+        {
+            case GameState.Running:
+                ResumeGame();
+                ResetTimer();
+                break;
+            case GameState.Victory:
+                PauseGame();
+                LoadVictoryScene();
+                break;
+            case GameState.GameOver:
+                PauseGame();
+                LoadGameOverScene();
+                break;
+        }
+    }
+
+    private void LoadVictoryScene()
+    {
+        SceneManager.LoadScene("Victory", LoadSceneMode.Additive);
+    }
+
+    private void ResetTimer()
+    {
+        _currentTime = timeToLose;
+    }
+
+    private void Update()
+    {
+        if (GameState == GameState.Running)
+        {
+            _currentTime -= Time.deltaTime;
+            if (_currentTime <= 0)
+            {
+                GameState = GameState.GameOver;
+            }
+        }
+
+        if (GameState == GameState.Victory)
+        {
+            if (Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                StartGame();
+            }
+        }
+
+        if (GameState == GameState.GameOver)
+        {
+            if (Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                GameState = GameState.Initialization;
+                ResumeGame();
+                LoadMainMenu();
+            }
+        }
+    }
+
+    private void LoadGameOverScene()
+    {
+        SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
+    }
+    
+    private void HandleGameMode(GameState obj)
+    {
+        GameState = obj;
+    }
+    
 }
